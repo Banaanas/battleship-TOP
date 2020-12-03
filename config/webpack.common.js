0ded693b283cf41ae27927cad148bd7dd5d3e4dd
Cyril <cyrildeveloppeur@gmail.com>
2020-12-03 11:13:49 +0100

    Favicon.ico

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -37,7 +37,7 @@ module.exports = {
     // Generates deprecation warning: https://github.com/jantimon/html-webpack-plugin/issues/1501
     new HtmlWebpackPlugin({
       title: "Battleship",
-      favicon: paths.src + "/images/favicon.svg",
+      favicon: paths.src + "/images/favicon.ico",
       template: paths.src + "/template.html", // template file
       filename: "index.html", // output file
     }),
